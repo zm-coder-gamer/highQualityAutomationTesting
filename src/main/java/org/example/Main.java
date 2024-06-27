a23bf58343f43aceb81c64a46961acff690833a3
Ziad Malik <z.malik@surreyschools.ca>
2024-06-27 13:41:05 -0700

    Added a meaningful test for some basic calc code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Main main = new Main();
@@ -7,7 +9,14 @@ public class Main {
     }
 
     private void start() {
-        System.out.println("Hello world! from start");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input 2 integers");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println("Starting Calculator");
+        Calc calc = new Calc();
+        System.out.println(calc.add(num1, num2));
     }
+
 
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -3,15 +3,13 @@ package org.example;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
 public class MainTest {
     @Test
     public void verifyMain() {
-        int i = 4;
-        int j = 6;
-        int sum = j+i;
-        Assert.assertEquals(sum,10);
+        Calc calc = new Calc();
+        int expected = 3;
+        int actual = calc.add(1, 2);
+        Assert.assertEquals(actual, expected, "sum is unexpected");
 
     }
 }
